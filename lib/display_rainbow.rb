4b40bd658cedb3e4960a5ea93b09e52009ee531a
Aristides Legakis <legakisnyc@gmail.com>
2018-08-11 17:41:19 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+
+
+bob = ["bob", 46, "juggler"]
+
+
+# "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+
+puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
